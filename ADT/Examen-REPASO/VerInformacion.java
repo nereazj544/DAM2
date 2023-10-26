b66bfc4c4f641f6ded05e4a44a49c5e36591ed7d
Nerea ZJ <117531760+nereazj544@users.noreply.github.com>
2023-10-26 15:59:17 +0200

    Ver informacion del directorio actual
    Al ejecutarlo se ven los elementos que se encuentran dentro de la ruta (Que se almacena dentro del String). 
    Por consola muestra lo siguiente:
    - Nombre (del archivo o fichero)
    - ¿Fichero?: Sí es un fichero mostra: true, en caso contrario mostrarta: False
    - ¿Directorio?: Sí es un directorio mostra: true, en caso contrario mostrarta: False

---
+++ ADT/Examen-REPASO/VerInformacion.java
@@ -0,0 +1,27 @@
+package REPASO.ADT;
+
+import java.io.File;
+import java.util.Scanner;
+
+public class VerInformacion {
+    public static void main(String[] args) {
+        System.out.println("Ruta del directorio: ");
+        Scanner sc = new Scanner(System.in);
+        String ruta =  sc.nextLine();
+        System.out.println("Informacion sobre el fichero (actual): " + ruta);
+        // String ruta = "src\\REPASO\\ADT";
+        File file = new File(ruta);
+
+        if (file.exists()) {
+            System.out.println("Nombre: " + file.getName());
+            System.out.println("Ruta: " + file.getPath()); // Ruta relativa
+            System.out.println("Ruta: " + file.getAbsolutePath()); // Ruta absoluta
+            System.out.println("¿Leer?: " + file.canRead()); // ¿se puede leer?
+            System.out.println("¿Escribir?: " + file.canWrite()); // ¿se puede escribir?
+            System.out.println("Tamaño: " + file.length()); // Tamaño
+            System.out.println("¿Directorio?: " + file.isDirectory()); // ¿Es un directorio?
+            System.out.println("¿Fichero?: " + file.isFile()); // ¿Es un fichero?
+            System.out.println("Nombre del dri.Padre: " + file.getParent()); //Directorio padre
+        }
+    }
+}
